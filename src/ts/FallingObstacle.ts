7389f911c602504e987e2073024417ab0311eea0
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-04-02 14:32:30 +0200

    Init FallingObstacles

--- src/ts/FallingObstacle.ts
+++ src/ts/FallingObstacle.ts
@@ -12,17 +12,26 @@ export class FallingObstacle extends Circle {
         super(ctx, {
             x: 0,
             y: 0,
-        }, new Hsl(hue, randomInt(1, 100), randomInt(1, 100)), randomInt(settings.minRadius, settings.maxRadius));
+        }, new Hsl(0, 0, 0), 0);
         this.canvas = canvas;
         this.hue = hue
+        this.initValues();
+    }
+
+    private initValues() {
+        this.radius = randomInt(settings.minRadius, settings.maxRadius);
+        this.color = new Hsl(this.hue, randomInt(1, 100), randomInt(1, 100))
         this.speed = randomInt(settings.minSpeed, settings.maxSpeed);
         this.position.x = -this.radius;
-        this.position.y = randomInt(this.radius, this.canvas.height - this.radius)
+        this.position.y = randomInt(this.radius, this.canvas.height - this.radius);
     }
 
 
     update() {
         this.position.x += this.speed;
+        if (this.position.x >= this.canvas.width + this.radius) {
+            this.initValues();
+        }
     }
 
     animate() {
