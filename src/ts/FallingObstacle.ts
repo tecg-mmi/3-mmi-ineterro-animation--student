533bb30066d588b0ad4a4c025dcd78204d2d4a6f
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-04-02 09:34:37 +0200

    Initial commit

---
+++ src/ts/FallingObstacle.ts
@@ -0,0 +1,41 @@
+import {Circle} from "./framework25/shapes/Circle";
+import {Hsl} from "./framework25/colors/Hsl";
+import {randomInt} from "./framework25/helpers/random";
+import {settings} from "./settings";
+import {iAnimatable} from "./framework25/types/iAnimatable";
+
+export class FallingObstacle extends Circle implements iAnimatable {
+    private speed: number;
+    private canvas: HTMLCanvasElement;
+    private hue: number;
+
+    constructor(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, hue: number) {
+        super(ctx, {
+            x: 0,
+            y: 0
+        }, null, 0);
+        this.hue = hue;
+        this.canvas = canvas;
+        this.initValues();
+    }
+
+    update() {
+        this.position.y += this.speed;
+    }
+
+    animate() {
+        this.update();
+        if (this.position.y >= this.canvas.height + this.radius) {
+            this.initValues();
+        }
+        super.draw();
+    }
+
+    private initValues() {
+        this.position.y = -this.radius;
+        this.position.x = randomInt(this.radius, this.canvas.width - this.radius);
+        this.color = new Hsl(this.hue, randomInt(1, 100), randomInt(1, 100),);
+        this.radius = randomInt(settings.minRadius, settings.maxRadius);
+        this.speed = randomInt(settings.minSpeed, settings.maxSpeed);
+    }
+}
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,41 @@
-console.log("Hello");
+import {settings} from "./settings";
+import {randomInt} from "./framework25/helpers/random";
+import {FallingObstacle} from "./FallingObstacle";
+import {Animation} from "./framework25/Animation";
+
+export class Main {
+    private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
+    private hue: number = randomInt(1, 360);
+    private fallingObstacles: FallingObstacle[] = [];
+    private animation: Animation;
+
+    constructor() {
+        this.canvas = document.getElementById(settings.canvasID) as HTMLCanvasElement;
+        this.ctx = this.canvas.getContext("2d");
+        this.animation = new Animation(this.ctx, this.canvas, this.fallingObstacles)
+        this.resizeCanvas();
+        this.addEventListeners();
+        this.addFallingObstacles();
+        this.animation.start();
+    }
+
+    resizeCanvas() {
+        this.canvas.width = innerWidth;
+        this.canvas.height = innerHeight;
+    }
+
+    private addEventListeners() {
+        window.addEventListener("resize", () => {
+            this.resizeCanvas();
+        })
+    }
+
+    private addFallingObstacles() {
+        for (let i = 0; i < settings.maxFallingObstacles; i++) {
+            this.fallingObstacles.push(new FallingObstacle(this.ctx, this.canvas, this.hue));
+        }
+    }
+}
+
+new Main();
